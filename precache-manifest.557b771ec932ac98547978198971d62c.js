07660d50c69feaaa5228e11b9476aa8fa7477a68
Ruan Montelo <ruanmontelo@gmail.com>
2020-09-24 23:17:38 -0300

    Updates

---
+++ precache-manifest.557b771ec932ac98547978198971d62c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43478aa4d472753c0582a3f7946a841a",
+    "url": "/TypingSpeedTest/index.html"
+  },
+  {
+    "revision": "aaa4faed29b970f6e8f9",
+    "url": "/TypingSpeedTest/static/css/main.94721c36.chunk.css"
+  },
+  {
+    "revision": "8b26986ac81a87c3bec5",
+    "url": "/TypingSpeedTest/static/js/2.de32b79b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TypingSpeedTest/static/js/2.de32b79b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa4faed29b970f6e8f9",
+    "url": "/TypingSpeedTest/static/js/main.1f3c1630.chunk.js"
+  },
+  {
+    "revision": "f19864edec06db31857f",
+    "url": "/TypingSpeedTest/static/js/runtime-main.c7a404f1.js"
+  },
+  {
+    "revision": "f5a069d24c7855d24188c14f570b6a57",
+    "url": "/TypingSpeedTest/static/media/github.f5a069d2.svg"
+  }
+]);
