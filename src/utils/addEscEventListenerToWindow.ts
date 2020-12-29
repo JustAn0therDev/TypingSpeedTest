f0fee263ef2c521ce37c4287975dcc0c5269e100
Ruan Montelo <ruanmontelo@gmail.com>
2020-12-28 23:00:31 -0300

    Adding logic for buttons to change background and foreground styles and removing Reset button from page

--- src/utils/addEscEventListenerToWindow.ts
+++ src/utils/addEscEventListenerToWindow.ts
@@ -1,9 +1,8 @@
 export default function addEscEventListenerToWindow(): void {
     window.addEventListener('keyup', (evt: KeyboardEvent) => {
         if (evt.key === "Escape") {
-            // Ignoring this because "Element" type DOES have a click function.
-            // @ts-ignore
-            document.querySelector('#button-reset-typing-input-state')?.click();
+            const spanElement: HTMLSpanElement | null = document.querySelector('#span-reset-typing-input-state');
+            spanElement?.click();
         }
     });
 }
