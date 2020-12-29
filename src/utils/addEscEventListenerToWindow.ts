ec1eedee3f4dc43dc7bd92a8337e92eeb08bf757
Ruan Montelo <ruanmontelo@gmail.com>
2020-12-28 21:06:57 -0300

    Bumping script versions, changing styles, fixing css and added react-color package

--- src/utils/addEscEventListenerToWindow.ts
+++ src/utils/addEscEventListenerToWindow.ts
@@ -1,7 +1,9 @@
 export default function addEscEventListenerToWindow(): void {
     window.addEventListener('keyup', (evt: KeyboardEvent) => {
         if (evt.key === "Escape") {
-            document.querySelector('button')?.click();
+            // Ignoring this because "Element" type DOES have a click function.
+            // @ts-ignore
+            document.querySelector('#button-reset-typing-input-state')?.click();
         }
     });
 }
