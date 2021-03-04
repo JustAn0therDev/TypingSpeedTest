7341538da2e063e54c21c8192b830333807968e5
JustAn0therDev <ruanmontelo@gmail.com>
2021-03-04 13:28:23 -0300

    Fixing bug where the reset colors button would not reset the colors in the first click, fixing change colors button content behavior

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,13 +1,8 @@
-import './Footer.css';
 import React from 'react';
-import GitHubLogo from './github.svg';
+import GithubCorner from 'react-github-corner'
 
 export default function Footer(): JSX.Element {
     return (
-        <footer>
-            <a href="https://github.com/JustAn0therDev/TypingSpeedTest" 
-            target="_blank" 
-            rel="noopener noreferrer">GitHub:&nbsp;<img src={GitHubLogo} alt="Github Logo"></img></a>
-        </footer>
+        <GithubCorner href="https://github.com/JustAn0therDev/TypingSpeedTest" />
     )
 }
